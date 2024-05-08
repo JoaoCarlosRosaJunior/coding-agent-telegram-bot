f44ed0c99ff58459aa47e9456176ae8f99e8bac4
João Carlos da Rosa <joaocarlosdarosa@MacBook-Pro-de-Joao.local>
2024-05-08 18:47:09 -0300

    feat: adding context for RAG

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -4,12 +4,27 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 from openai import OpenAI
+import pandas as pd
+import numpy as np
+from questions import answer_question
 
 load_dotenv()  # take environment variables from .env.
+
+# Get the directory of the current Python script
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory
+parent_directory = os.path.abspath(os.path.join(current_file_directory, os.pardir))
 
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+df = pd.read_csv(parent_directory + '/lsu_pilot/processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      answer = answer_question(df, question=update.message.text, debug=True)
+      await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 messages = [{
   "role": "system",
@@ -46,8 +61,10 @@ if __name__ == '__main__':
 
   start_handler = CommandHandler('start', start)
   chat_handler = CommandHandler('chat', chat)
-
+  mozilla_handler = CommandHandler('mozilla', mozilla)
+  
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
+  application.add_handler(mozilla_handler)
 
   application.run_polling()
